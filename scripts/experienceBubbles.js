3939a7f20a225bca26913ab28ea9464171dc5297
mberk06 <michaelberk99@gmail.com>
2019-03-17 22:34:29 -0400

    Wordcloud done

--- scripts/experienceBubbles.js
+++ scripts/experienceBubbles.js
@@ -1,40 +1,28 @@
 //This file will be used to create bubbles with experience content
 //in the experience page
 
-//Read from text file
-/*var jsonText;
-$.getJSON('/text/experience_text.json', function(data) {         
-    jsonText = JSON.stringify(data);
-});
+//dummy data 
+var date = '2/2018';
+var title = 'NBA Lineup';
 
-//Note: size = 1-10
-var createDiv = function(size, title, description, date, duration) {
-	var r = String(size/10*40/2)+"%"; //set radius
-	var titleSize = String(size*16)+"px"; //set title size
+//////////////////////
+//Read in data
+//////////////////////
 
-	//add bubble
-	var bubble = document.createElement("div");
-	bubble.setAttribute("width", "300px");
-	bubble.setAttribute("height", "auto");
-	bubble.setAttribute("border-radius","50%");
-	bubble.setAttribute("background-color", "orange");
-	bubble.setAttribute("style", "background-color: orange;");
 
-	//add text to bubble
-	var t = document.createElement("h1");
-	t.textContent = title;
-	t.setAttribute("id", title+"_title");
-	t.setAttribute("style", "color: red;");
-	bubble.appendChild(t);
 
-	//add bubble to container
-	document.getElementById("bubble-container").append(bubble);
 
-}
+//////////////////////
+//Add content
+//////////////////////
+//Create bubble
 
-createDiv(10,"TEst","description","2-2018","2 years");
+//Create line with date
 
-$('#someid').click(function() {
-	alert("hi");
-	//$( this ).setAttribute("src","/assets/img/bubbleHover.png");
-});*/
+//Create tick marks above and below
+
+
+
+//////////////////////
+//
+//////////////////////
--- wordcloud/formatsvg.py
+++ wordcloud/formatsvg.py
@@ -3,16 +3,20 @@
 #########################
 #imports
 import re
+import csv
 import random
 
 #Constants
 col = "grey"
 className = "wordcloud-word"
 
-#Read in file as string
+#Read in svg as string
 svg = open("wordcloud-original.svg","r") 
 s = svg.read()
 svg.close()
+
+#read in words to replace
+wordLinks = list(csv.reader(open("word-links.csv")))
 
 #########################
 #replacement function
@@ -36,6 +40,13 @@ def replaceText(startString, endChar, replacement, s):
 #Call functions
 out = replaceText("fill:", ";", col, s) #Change color
 out = replaceText('"middle"', 't', ' class="'+className+'" onmouseover="darkWord(this)" onmouseleave="lightWord(this)" ', out) #Add class name
+
+#add links
+for word,url in wordLinks:
+	#index to add
+	if word in out:
+		temp = out.split(">"+word)
+		out = temp[0] + ' onclick="window.location=\''+url+'\'">' + word + temp[1]
 
 #write to svg
 svg = open("wordcloud.svg","w") 
