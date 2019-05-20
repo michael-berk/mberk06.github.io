ee12bfca7baa9e3cd9fc03c0e320c32c7870bef2
mberk06 <michaelberk99@gmail.com>
2019-05-20 12:39:30 -0700

    About and EAbout and experience overhaul

--- scripts/becauseButtons.js
+++ scripts/becauseButtons.js
@@ -30,7 +30,9 @@ $( document ).ready(function() {
 var initButtons = function(text) {
 	$("#because-text1").text(text[0]);
 	$("#because-text2").text(text[1]);
-	$("#b_clutter").css("color", "red");
+	$("#b_clutter").css("border-color", "#1D2041");
+	$("#b_improvement").css("color", "grey");
+	$("#b_money").css("color", "grey");
 }
 
 var toggleButtons = function(buttonNum, v) {
@@ -39,12 +41,14 @@ var toggleButtons = function(buttonNum, v) {
 	var buttons = ["clutter","improvement","money"];
 	var buttonPressed = buttons.splice(buttonNum-1,1);
 
-	$("#b_"+buttonPressed).css('color','red'); //change color
+	$("#b_"+buttonPressed).css('color','#1D2041'); //change color
+	$("#b_"+buttonPressed).css('border-color','#1D2041'); //change color
 	$("#because-text1").text(texts[buttonNum-1][0]); //render new text
 	$("#because-text2").text(texts[buttonNum-1][1]); //render new text
 	
 	//toggle other button color
 	for (i = 0; i < 2; i++) {
-		$("#b_"+buttons[i]).css('color','black');
+		$("#b_"+buttons[i]).css('color','grey');
+		$("#b_"+buttons[i]).css('border-color','grey');
 	}
 }
--- scripts/wordcloud.js
+++ scripts/wordcloud.js
@@ -1,6 +1,6 @@
 //word cloud testing
 var darkWord = function(elem) {
-	elem.style.fill = "black";
+	elem.style.fill = "#1D2041";
 };
 
 var lightWord = function(elem) {
